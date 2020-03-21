d989f7de026b787277a71a808f607e5ec54a5f60
pratham022 <59230404+pratham022@users.noreply.github.com>
2020-03-22 00:07:10 +0530

    HelloWorld file created

---
+++ HelloWorld.asm
@@ -0,0 +1,16 @@
+section .data
+    msg db 'Hello World!', 0xa
+    len equ $- msg
+    
+section .text
+    global _start:
+    _start:
+      
+            mov edx, len    ; message length
+            mov ecx, msg    ; reference of msg
+            mov ebx, 1      ; file descriptor (stdout)
+            mov eax, 4      ; sys_write call
+            int 0x80        ; interrupt call
+            
+            mov eax, 1      ; sys_exit  call number
+            int 0x80        ; call interrupt(kernel)
