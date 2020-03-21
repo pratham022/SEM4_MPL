67715aac731c88100d1ade01b24ad2c2bed1ac88
pratham022 <59230404+pratham022@users.noreply.github.com>
2020-03-22 00:10:19 +0530

    Output added

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -14,3 +14,5 @@ section .text
             
             mov eax, 1      ; sys_exit  call number
             int 0x80        ; call interrupt(kernel)
+            
+            ; "Hello World!" will be printed on screen
