b7f4322e2d8a87f48878336a12c82c98952ddf2f
pratham022 <59230404+pratham022@users.noreply.github.com>
2020-03-22 09:55:21 +0530

    output added

--- Hello64.asm
+++ Hello64.asm
@@ -8,10 +8,16 @@ section .text
 	
 		mov rax, 1		  ; system call for write
 		mov rdi, 1		  ; stdout file
-		mov rsi, msg		; reference of 'Hello World!'
-		mov rdx, len		; size of msg
-		syscall			    ; interrupt call
+		mov rsi, msg		  ; reference of 'Hello World!'
+		mov rdx, len		  ; size of msg
+		syscall			  ; interrupt call
 		
 		mov rax, 60		  ; sys_exit
-		xor rdi, rdi		; exit_success
-		syscall			    ; interrupt
+		xor rdi, rdi		  ; exit_success
+		syscall			  ; interrupt
+
+
+; prathamesh@prathamesh-Inspiron-3576:~/Documents/git_MPL$ nasm -f elf64 Hello64.asm
+; prathamesh@prathamesh-Inspiron-3576:~/Documents/git_MPL$ ld -o a1 Hello64.o
+; prathamesh@prathamesh-Inspiron-3576:~/Documents/git_MPL$ ./a1
+; Hello World!
